dca1a6f37bb31b9ddb446868b7c02487a8095240
‘Juliano <julianoperins@gmail.com>
2021-03-25 09:59:24 -0400

    set up server

--- server/index.js
+++ server/index.js
@@ -3,4 +3,24 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-const express = require("express");
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://julianoperin:julianoperin123@spot-cluster.7pqgw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${5000}`));
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+mongoose.set("useFindAndModify", false);
